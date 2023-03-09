02705022be0b3a746ed7f3696808939f36817f68
AstonDV <astondv.kms@gmail.com>
2023-03-09 22:14:36 +1000

    Добавлен метод отбора строк из массива

--- Program.cs
+++ Program.cs
@@ -31,12 +31,31 @@ int SearchSize(string[] array)
     return count;
 }
 
+string[] ArraySorting (string[] array, int size)
+{
+    string[] newArray = new string[size];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) 
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    return newArray;
+}
+
+
+
 void main()
 {
     Console.Clear();
     string input = StringsInput("Введите строки через пробел или запятую");
     string[] inputArray = InputArray(input);
     int finalArraySize = SearchSize(inputArray);
+    string[] finalArray = ArraySorting(inputArray, finalArraySize);
+
     Console.WriteLine(finalArraySize);
 }
 
